b26a6ae622b4eae18f9eb52a49d325e5e01002af
Jeff Aigner <enumeration@gmail.com>
2012-12-14 00:02:29 -0600

    Refactoring the rpc test

--- rpctest.go
+++ rpctest.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/rpc"
 )
@@ -22,11 +23,13 @@ type Quotient struct {
 
 type Arith int
 
+// example rpc function that multiplies A and B
 func (t *Arith) Multiply(args *Args, reply *int) error {
 	*reply = args.A * args.B
 	return nil
 }
 
+// example rpc function that divides A by B
 func (t *Arith) Divide(args *Args, quo *Quotient) error {
 	if args.B == 0 {
 		return errors.New("divide by zero")
@@ -36,38 +39,60 @@ func (t *Arith) Divide(args *Args, quo *Quotient) error {
 	return nil
 }
 
+// DialHTTPS connects to an HTTPS RPC server at the specified network address
+// listening on the default HTTP RPC path using tls.Config config
+func DialHTTPS(network, address string, config *tls.Config) (*rpc.Client, error) {
+	return DialHTTPSPath(network, address, rpc.DefaultRPCPath, config)
+}
+
+// DialHTTPSPath connects to an HTTPS RPC server
+// at the specified network address and path using tls.Config config
+func DialHTTPSPath(network, address, path string, config *tls.Config) (*rpc.Client, error) {
+	var err error
+	conn, err := tls.Dial(network, address, config)
+	if err != nil {
+		return nil, err
+	}
+	io.WriteString(conn, "CONNECT "+path+" HTTP/1.0\n\n")
+
+	// Require successful HTTP response
+	// before switch to RPC protocol.
+	resp, err := http.ReadResponse(bufio.NewReader(conn), &http.Request{Method: "CONNECT"})
+	if err == nil && resp.StatusCode == http.StatusOK {
+		return rpc.NewClient(conn), nil
+	}
+	if err == nil {
+		err = errors.New("unexpected HTTP response: " + resp.Status)
+	}
+	conn.Close()
+	return nil, &net.OpError{
+		Op:   "dial-http",
+		Net:  network + " " + address,
+		Addr: nil,
+		Err:  err,
+	}
+}
+
+// example rpc client
 func test_client() {
 	cert, err := tls.LoadX509KeyPair("certs/client.pem", "certs/client.key")
 	if err != nil {
 		log.Fatal("client: loadkeys: ", err)
 	}
-	fmt.Println("loaded client certs...")
 	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
-	conn, err := tls.Dial("tcp", "localhost:1234", &config)
+
+	client, err := DialHTTPS("tcp", "localhost:1234", &config)
+
 	if err != nil {
 		log.Fatal("client: dial: ", err)
 	}
-	fmt.Println("Dialed in...")
-	defer conn.Close()
 
-	io.WriteString(conn, "CONNECT "+rpc.DefaultRPCPath+" HTTP/1.0\n\n")
-	resp, err := http.ReadResponse(bufio.NewReader(conn), &http.Request{Method: "CONNECT"})
-	if err != nil {
-		log.Fatal("client: response: ", err)
-	}
-	fmt.Println("response: ", resp)
-
-	client := rpc.NewClient(conn)
-	fmt.Println("Created client: ", client)
 	args := &Args{7, 8}
 	var reply int
-	fmt.Println("Making call...")
 	err = client.Call("Arith.Multiply", args, &reply)
-	fmt.Println("Returned?")
 	if err != nil {
 		log.Fatal("arith error:", err)
 	}
-	fmt.Println("Made call!")
 	fmt.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
 }
 
